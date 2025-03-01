dce13f06867cd64e024eb433f9c8cae288af53c6
lemira <lemirat@gmail.com>
2025-03-01 20:35:58 +0100

    Update view.html.php

--- admin/views/controlpanel/view.html.php
+++ admin/views/controlpanel/view.html.php
@@ -1,1 +1,10 @@
-
+<?php
+defined('_JEXEC') or die;
+class Posts2articleViewControlpanel extends JViewLegacy
+{
+    public function display($tpl = null)
+    {
+        JToolbarHelper::title('Posts2Article: Control Panel', 'puzzle');
+        parent::display($tpl);
+    }
+}
