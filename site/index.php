86773f5d513cb329a625831f73cfb1e7d3bfb36c
lemira <lemirat@gmail.com>
2025-03-01 19:45:15 +0100

    Update index.php

--- site/index.php
+++ site/index.php
@@ -1,1 +1,10 @@
-
+<?php
+defined('_JEXEC') or die;
+require_once JPATH_COMPONENT . '/controller.php';
+JLoader::register('Posts2articleController', JPATH_COMPONENT . '/controller.php');
+use Joomla\CMS\Factory;
+$app = Factory::getApplication();
+$input = $app->input;
+$controller = JControllerLegacy::getInstance('Posts2article');
+$controller->execute($input->getCmd('task', 'display'));
+$controller->redirect();
