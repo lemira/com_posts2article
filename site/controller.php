ab8b44ecbd8b2aa935afa5383cfd47c89da85539
lemira <lemirat@gmail.com>
2025-03-01 19:44:52 +0100

    Update controller.php

--- site/controller.php
+++ site/controller.php
@@ -1,1 +1,10 @@
-
+<?php
+defined('_JEXEC') or die;
+class Posts2articleController extends JControllerLegacy
+{
+    public function display($cachable = false, $urlparams = [])
+    {
+        $this->input->set('view', 'params');
+        parent::display($cachable, $urlparams);
+    }
+}
